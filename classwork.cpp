ae89128dd1a88deef037a5d15cc57497eeb2f89e
AnnaN225 <annanguyen102207@gmail.com>
2024-12-11 21:49:44 +0000

    variables

--- classwork.cpp
+++ classwork.cpp
@@ -3,10 +3,18 @@
 
 int main()
 {
-  int age;
-  age = 17;
+  //SET age TO 10
+  int age = 10;
   double price = 19.99;
-  numPets = 0;
-  grade = 11;
+  bool hasPets = true;
+
+// \n is a new line
+// std::endl is a new line
+// std::cout << computer output
+  std::cout << age << "\n" << price << std::endl << hasPets << std::endl;
+
+  std::cout << "I am " << age << " years old.\n";
+  std: cout <<"I have " << price << " dollars.\n";
+  
   return 0;
 }
